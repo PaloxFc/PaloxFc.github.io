b990f2e123a735ecbf08fc267a7b590d308298fd
PaloxFc <167017295+PaloxFc@users.noreply.github.com>
2024-05-01 22:27:06 +0100

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,9 @@
-
+function redirectToSelected() {
+            var selectElement = document.getElementById("socialMediaSelect");
+            var selectedOption = selectElement.options[selectElement.selectedIndex];
+            if (selectedOption.value) {
+                window.open(selectedOption.value, "_blank");
+            } else {
+                alert("Please select a social media platform.");
+            }
+        }
